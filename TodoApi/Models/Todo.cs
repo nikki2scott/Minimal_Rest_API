bd60e829dc05bb751a8755a5b484e3c53af4c557
Melissa Hegney <mhegney@stmartin.edu>
2025-07-09 19:09:30 -0700

    complete todo

--- TodoApi/Models/Todo.cs
+++ TodoApi/Models/Todo.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models;
 
 /// <summary>
@@ -7,7 +9,9 @@ namespace TodoApi.Models;
 /// </summary>
 public class Todo
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
